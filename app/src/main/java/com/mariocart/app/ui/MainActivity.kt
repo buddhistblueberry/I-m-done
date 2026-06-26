b5843260f2e877957c31c13aa36b522e6512a708
ashtonhardy555-stack <ashtonhardy555@gmail.com>
2026-06-26 01:30:37 -0400

    Update MainActivity.kt

--- app/src/main/java/com/mariocart/app/ui/MainActivity.kt
+++ app/src/main/java/com/mariocart/app/ui/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.mariocart.app.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mariocart.app.data.server.StreamExtractor
 import com.mariocart.app.ui.home.HomeScreen
 import com.mariocart.app.ui.player.PlayerActivity
 import com.mariocart.app.ui.theme.MarioCartTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -30,40 +23,19 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     HomeScreen(
-                        onMovieClick = { tmdbId ->
-                            startActivity(
-                                PlayerActivity.newIntent(
-                                    context = this,
-                                    tmdbId = tmdbId,
-                                    contentType = "movie"
-                                )
+                        onItemClick = { tmdbId, contentType, season, episode ->
+                            val intent = PlayerActivity.newIntent(
+                                context = this,
+                                tmdbId = tmdbId,
+                                contentType = contentType,
+                                season = season ?: 1,
+                                episode = episode ?: 1
                             )
-                        },
-                        onTvClick = { tmdbId, season, episode ->
-                            startActivity(
-                                PlayerActivity.newIntent(
-                                    context = this,
-                                    tmdbId = tmdbId,
-                                    contentType = "tv",
-                                    season = season,
-                                    episode = episode
-                                )
-                            )
+                            startActivity(intent)
                         }
                     )
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainPreview() {
-    MarioCartTheme {
-        HomeScreen(
-            onMovieClick = {},
-            onTvClick = { _, _, _ -> }
-        )
     }
 }
