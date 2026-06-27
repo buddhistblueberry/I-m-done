aeaec408e3bbf41c8c18b3dac516fcba70b2d81c
ashtonhardy555-stack <ashtonhardy555@gmail.com>
2026-06-27 01:27:06 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,3 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://chaquo.com/maven") }
-    }
-}
