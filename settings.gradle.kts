d68d6220194e84a40135c0f862698f711535c226
ashtonhardy555-stack <ashtonhardy555@gmail.com>
2026-07-12 03:42:52 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,12 +7,12 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)  // Changed from FAIL_ON_PROJECT_REPOS
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "MarioCart"
+rootProject.name = "I-m-done"   // or MarioCart if you prefer
 include(":app")
