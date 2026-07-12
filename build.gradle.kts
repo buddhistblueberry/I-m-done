daf134809c51687faeed6d3b43b85aa217783009
ashtonhardy555-stack <ashtonhardy555@gmail.com>
2026-07-12 07:24:35 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
+// Top-level build file
+
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    id 'com.android.application' version '8.5.0' apply false
+    id 'org.jetbrains.kotlin.android' version '2.0.20' apply false
+    id 'com.chaquo.python' version '15.0.1' apply false
+    id 'org.jetbrains.kotlin.plugin.compose' version '2.0.20' apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }
