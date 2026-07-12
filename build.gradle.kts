b4b212164c79a125136fc2c76df97aa0652ae8b9
ashtonhardy555-stack <ashtonhardy555@gmail.com>
2026-07-12 03:37:24 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,7 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:8.5.0'
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20'
-    }
-}
-
 plugins {
-    id 'org.jetbrains.kotlin.plugin.compose' version '2.0.20' apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
 
 allprojects {
